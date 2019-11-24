53972836171fd2f20ae353cadd0a25143d77e06b
guoyu <guoyumail@qq.com>
2019-11-24 23:35:34 +0800

    Routing definition

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate diesel;
 extern crate serde;
 
 use actix_files as fs;
-use actix_web::{middleware, App, HttpServer, web};
+use actix_web::{middleware, App, HttpServer, web, Scope};
 
 mod model;
 mod api;
@@ -14,17 +14,29 @@ fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_server=debug,actix_web=debug");
     env_logger::init();
 
-    HttpServer::new(move||{
+    HttpServer::new(move || {
         App::new()
             .data(util::DataBase::new("jygo.db")) // 数据库
             .wrap(middleware::Logger::default()) // 日志
             .wrap(middleware::DefaultHeaders::new().header("content-type", "appliction/json;charset=utf-8"))
-            .service(
-                web::scope("/user") // 用户接口
-                .route("/list",web::get().to(api::user_api::list)) // 查询用户
-                .route("/save",web::post().to(api::user_api::save))) // 保存用户
+            .service(route())
             .service(fs::Files::new("/", "static/").index_file("index.html"))
     })
-    .bind("0.0.0.0:8080").expect("端口可能被占用了!")
-    .run()
+        .bind("0.0.0.0:8080").expect("端口可能被占用了!")
+        .run()
+}
+
+/// Routing definition; 路由定义
+fn route() -> Scope {
+    web::scope("/rust_web")
+        .service(
+            web::scope("/user") // user api
+                .route("/list", web::get().to(api::user_api::list)) // query
+                .route("/save", web::post().to(api::user_api::save)) // save
+        )
+        .service( // scope demo
+            web::scope("/user2") // user api
+                .route("/list", web::get().to(api::user_api::list)) // query
+                .route("/save", web::post().to(api::user_api::save)) // save
+        )
 }
